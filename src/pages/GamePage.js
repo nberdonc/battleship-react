80a5eeac2e49bd337dd1bb79c21b8e227a203f62
Nuria Berdonces <b_nuria@hotmail.com>
2021-08-18 13:20:18 +0200

    async off

--- src/pages/GamePage.js
+++ src/pages/GamePage.js
@@ -5,7 +5,9 @@ import './GamePage.css';
 const GamePage = () => {
     let [playerBoard, setPlayerBoard] = useState([])
     let [PcBoard, setPcBoard] = useState([])
-    let [turn, setTurn] = useState("YOUR")
+    let [startGame, setStartGame] = useState("off")
+    let [placedShips, setPlacedShips] = useState("no")
+    let [turn, setTurn] = useState("")
     let cols = 10;
     let rows = 10;
 
@@ -16,7 +18,7 @@ const GamePage = () => {
 
     }, [])
 
-    /////Player board/////
+    /////Player board///// to change for drag and drop
     const [rowInput, setRowInput] = useState()
     const [columnInput, setColumnInput] = useState()
 
@@ -45,18 +47,21 @@ const GamePage = () => {
     //Player: shoots to coordinates on PC's board
 
     let recieveShootPc = (rowIndex, colIndex, e) => {
-        if (PcBoard[rowIndex][colIndex] === 1) {
-            PcBoard[rowIndex][colIndex] = 'x'
-            console.log("hit")
-            console.log('row', rowIndex)
-            console.log('col', colIndex)
-            console.log(PcBoard)
-        } else {
-            PcBoard[rowIndex][colIndex] = 'o'
-            console.log("miss")
-            console.log('row', rowIndex)
-            console.log('col', colIndex)
-            console.log(PcBoard)
+        if (startGame === "on") {
+            if (PcBoard[rowIndex][colIndex] === 1) {
+                PcBoard[rowIndex][colIndex] = 'x'
+                console.log("hit")
+                console.log('row', rowIndex)
+                console.log('col', colIndex)
+                console.log(PcBoard)
+            } else {
+                PcBoard[rowIndex][colIndex] = 'o'
+                console.log("miss")
+                console.log('row', rowIndex)
+                console.log('col', colIndex)
+                console.log(PcBoard)
+            }
+            setTurn("PC's")
         }
     };
 
@@ -70,26 +75,38 @@ const GamePage = () => {
     const placeShipsRandomly = () => {
         getRandomCoordinates()
         PcBoard[rowEnemy - 1][colEnemy - 1] = 1
+        setPlacedShips("yes")
         console.log("PcBoard", PcBoard)
     };
 
     //PC: shoots random to player's board
     let recieveShootPlayer = (rowIndex, colIndex, e) => {
-        if (playerBoard[rowIndex][colIndex] === 1) {
-            playerBoard[rowIndex][colIndex] = 'x'
-            console.log("hit")
-            console.log('row', rowIndex)
-            console.log('col', colIndex)
-            console.log(playerBoard)
-        } else {
-            playerBoard[rowIndex][colIndex] = 'o'
-            console.log("miss")
-            console.log('row', rowIndex)
-            console.log('col', colIndex)
-            console.log(playerBoard)
+        if (startGame === "on") {
+            if (playerBoard[rowIndex][colIndex] === 1) {
+                playerBoard[rowIndex][colIndex] = 'x'
+                console.log("hit")
+                console.log('row', rowIndex)
+                console.log('col', colIndex)
+                console.log(playerBoard)
+            } else {
+                playerBoard[rowIndex][colIndex] = 'o'
+                console.log("miss")
+                console.log('row', rowIndex)
+                console.log('col', colIndex)
+                console.log(playerBoard)
+            }
+            setTurn("YOUR")
         }
     };
 
+    //Start Game Function
+    let startGameBtn = (e) => {
+        if (placedShips === "yes") {
+            setStartGame("on")
+            setTurn("YOUR")
+        }
+
+    }
 
     return (
         <div className="">
@@ -98,17 +115,18 @@ const GamePage = () => {
             <input type="text" name="row" placeholder="row" value={rowInput} onChange={handleRowInput} />
             <input type="text" name="column" placeholder="column" value={columnInput} onChange={handleColumnInput} />
             <button onClick={placeShips}>PLACE SHIP</button>
-            <div className="boards">
+
+            <div className="all-boards">
                 <div>
                     <p>PLAYER BOARD</p>
-                    <div className="boards-each">
-                        <div >
+                    <div>
+                        <div className="grid">
                             {playerBoard.map((rows, rowIndex) => (
                                 <div key={rowIndex} >
                                     {
                                         rows.map((cols, colIndex) => (
-                                            <button key={colIndex} className="square"
-                                                onClick={e => recieveShootPlayer(rowIndex, colIndex, e)} />
+                                            <div key={colIndex} className="square"
+                                                onClick={e => recieveShootPlayer(rowIndex, colIndex, e)}></div>
                                         ))}
                                 </div>
                             ))}
@@ -118,19 +136,50 @@ const GamePage = () => {
                 <h4>{turn} TURN</h4>
                 <div>
                     <p>PC BOARD</p>
-                    <div className="boards-each">
-                        <div >
+                    <div>
+                        <div className="grid">
                             {PcBoard.map((rows, rowIndex) => (
                                 <div key={rowIndex} >
                                     {
                                         rows.map((cols, colIndex) => (
-                                            <button key={colIndex} className="square"
-                                                onClick={e => recieveShootPc(rowIndex, colIndex, e)} />
+                                            <div key={colIndex} className="square"
+                                                onClick={e => recieveShootPc(rowIndex, colIndex, e)}></div>
                                         ))}
                                 </div>
                             ))}
                         </div>
                     </div>
+                </div>
+            </div>
+            <button onClick={startGameBtn}>START</button>
+            <h4 className="info"></h4>
+            <div className="ships">
+                <div className="ship destroyer-container" draggable="true">
+                    <div id="destroyer-0"></div>
+                    <div id="destroyer-1"></div>
+                </div>
+                <div className="ship submarine-container" draggable="true">
+                    <div id="submarine-0"></div>
+                    <div id="submarine-1"></div>
+                    <div id="submarine-2"></div>
+                </div>
+                <div className="ship cruiser-container" draggable="true">
+                    <div id="cruiser-0"></div>
+                    <div id="cruiser-1"></div>
+                    <div id="cruiser-2"></div>
+                </div>
+                <div className="ship battleship-container" draggable="true">
+                    <div id="battleship-0"></div>
+                    <div id="battleship-1"></div>
+                    <div id="battleship-2"></div>
+                    <div id="battleship-3"></div>
+                </div>
+                <div className="ship carrier-container" draggable="true">
+                    <div id="carrier-0"></div>
+                    <div id="carrier-1"></div>
+                    <div id="carrier-2"></div>
+                    <div id="carrier-3"></div>
+                    <div id="carrier-4"></div>
                 </div>
             </div>
         </div>
