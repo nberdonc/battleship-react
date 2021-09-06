28c90b0cdac30531e1b0eb4f7a17efbaa8972231
Nuria Berdonces <b_nuria@hotmail.com>
2021-09-06 22:41:48 +0200

    rotate fixing bug

--- src/components/ShipsDragNDrop.js
+++ src/components/ShipsDragNDrop.js
@@ -4,6 +4,9 @@ export const ShipsDragNDrop = ({ shipList }) => {
     let offSetPartition = 40 //each ship partition size is 40px
     let rotated = false;
     const handleDragStart = (e) => {
+        console.log("ASDASD", document.querySelector(`.${e.target.className}`))
+        e.dataTransfer.setDragImage(document.querySelector(`.${e.target.className}`), 20, 20)
+
         const data = JSON.stringify({
             size: e.target.value,
             offsetY: Math.floor(
@@ -15,16 +18,17 @@ export const ShipsDragNDrop = ({ shipList }) => {
             rotated: rotated,
             name: e.target.name,
         });
+
         e.dataTransfer.setData("ship", data);
     }
 
     const handleClick = (e) => {
         if (!rotated) {
-            e.target.parentNode.style.transform = "rotate(90deg)";
-            //document.querySelector(`.${e.target.className}`).style.transform = "rotate(90deg)";
+            // e.target.parentNode.style["background-color"] = "red";
+            document.querySelector(`.${e.target.className}`).style.transform = "rotate(90deg)";
         } else {
-            e.target.parentNode.style.transform = "";
-            //document.querySelector(`.${e.target.className}`).style.transform = "";
+            //e.target.parentNode.style.transform = "";
+            document.querySelector(`.${e.target.className}`).style.transform = "";
         }
         rotated = !rotated;
     };
@@ -34,17 +38,20 @@ export const ShipsDragNDrop = ({ shipList }) => {
     return (
         <div className="ships-container">
             {shipList.map((ele, eleIdx) => (
+                /*<div draggable="true">*/
                 <button
                     key={eleIdx}
                     onDragStart={handleDragStart}
-                    draggable="true"
                     className={ele.name}
                     name={ele.name}
                     value={ele.length}
                     onClick={(e) => handleClick(e)} />
+                /* </div>*/
             ))}
         </div>
     )
 }
-
+/* crear classe nova, fent els barcos amples i llargs
+per ferlo vertical i afegir classname al button//
+o fer toggle per canviar de classname onlclick*/
 
