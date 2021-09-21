c7a541696c38657e46006877c6f4b60fc80e8008
Nuria Berdonces <b_nuria@hotmail.com>
2021-09-21 17:49:17 +0200

    fix bable.jest bug

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,6 @@
-module.exports = {
-    presets: [
-        ['@babel/preset-env', { targets: { node: 'current' } }],
-        ['@babel/preset-react', { targets: { node: 'current' } }]
-    ]
-};
+//module.exports = {
+//     presets: [
+//         ['@babel/preset-env', { targets: { node: 'current' } }],
+//         ['@babel/preset-react', { targets: { node: 'current' } }]
+//     ]
+// };
